2f130fe15e0c1d4cb5bc70e6a567ab0dcfd1a085
Michael Anderson <michael@anderson-clan.org>
2021-08-16 21:34:05 -0500

    Added swipe right/left to change periods for touch

--- webclient/src/components/TransactionList.vue
+++ webclient/src/components/TransactionList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="uk-section uk-overflow-auto" uk-grid>
+    <div class="uk-section uk-overflow-auto" uk-grid @touchmove="onTouch" @touchend="onTouchEnd">
         <div class="uk-container uk-width-2-3@m" uk-height-viewport="expand: true" id="viewport">
             <div>
                 <transaction-detail v-for="transaction in periodTransactions" 
@@ -16,8 +16,11 @@ import Vuex from 'vuex';
 import TransactionDetail from './TransactionDetail.vue';
 import TransactionInfo from './TransactionInfo.vue';
 import { Currency } from '../util/formats';
+import { CalculatePeriod } from '../util/date';
 
 Vue.use(Vuex);
+
+var lastTouch = null;
 
 export default {
     components: {
@@ -32,7 +35,30 @@ export default {
         currency(value) { return Currency.format(value); },
         info(transaction) {
             this.selected = transaction;
-        }
+        },
+        onTouch(evt) {
+            if (lastTouch !== null) {
+                let deltaX = lastTouch.clientX - evt.touches[0].clientX;
+                if (Math.abs(deltaX) > this.$el.offsetWidth / 2) {
+                    if (deltaX > 0) { // increase by 1 period
+                        let period = CalculatePeriod(this.$store.state.period.end.plus({days: 1}), 
+                            this.$store.state.config.startDate, this.$store.state.config.periodLength);
+                        this.$store.commit('set', { key: 'period', value: period });
+                    } else { // decrease by 1 period
+                        let period = CalculatePeriod(this.$store.state.period.start.minus({days: 1}), 
+                            this.$store.state.config.startDate, this.$store.state.config.periodLength);
+                        this.$store.commit('set', { key: 'period', value: period });
+                    }
+                    // ensure we don't keep swiping
+                    lastTouch = null;
+                }
+            } else {
+                lastTouch = evt.touches[0];
+            }
+        },
+        onTouchEnd() {
+            lastTouch = null;
+        },
     },
     computed: {
         ...Vuex.mapGetters(['periodTransactions']),
